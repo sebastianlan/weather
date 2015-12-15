0f73c10b3144381480ea01f8d8fbe2742f69aca3
sebastianlan <sebastianlan.original@gmail.com>
2015-12-15 22:11:57 +0800

    modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -31,8 +31,6 @@ gulp.task('browser-sync', function() {
     });
 });
 
-
-// 样式
 gulp.task('styles', function() {  
   return gulp.src('src/styles/main.scss')
     .pipe(sass({ style: 'expanded', }))
@@ -44,7 +42,6 @@ gulp.task('styles', function() {
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
-// 脚本
 gulp.task('scripts', function() {  
   return gulp.src('src/scripts/**/*.js')
     .pipe(jshint('.jshintrc'))
@@ -57,7 +54,6 @@ gulp.task('scripts', function() {
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
-// 图片
 gulp.task('images', function() {  
   return gulp.src('src/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
@@ -65,47 +61,26 @@ gulp.task('images', function() {
     .pipe(notify({ message: 'Images task complete' }));
 });
 
-// 清理
 gulp.task('clean', function() {  
   return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false})
     .pipe(clean());
 });
 
-// 预设任务
 gulp.task('default', ['clean'], function() {  
     gulp.start('styles', 'scripts', 'images');
 });
 
-// 看手
 gulp.task('watch', function() {
-
-  // 看守所有.scss档
   gulp.watch('src/styles/**/*.scss', ['styles']);
-
-  // 看守所有.js档
   gulp.watch('src/scripts/**/*.js', ['scripts']);
-
-  // 看守所有图片档
   gulp.watch('src/images/**/*', ['images']);
-
-  // 建立即时重整伺服器
   var server = livereload();
-
-  // 看守所有位在 dist/  目录下的档案，一旦有更动，便进行重整
   gulp.watch(['dist/**']).on('change', function(file) {
     server.changed(file.path);
   });
 
 });
 
-
-gulp.task('normal', function() {
-  gulp.src(['*.html']).pipe(reload({stream:true}));
-});
-
-// 在命令行中运行 gulp，默认执行 default，你也可以自定义其他命令
 gulp.task('default', ['compass', 'browser-sync','normal'], function () {
   gulp.watch(['./sass/{,*/}*.scss'], ['compass']);
   gulp.watch(['./*.html','./css/{,*/}*.css','./js/{,*/}*.js','./images/{,*/}*.{png,jpg}'],['normal']);
-
-
